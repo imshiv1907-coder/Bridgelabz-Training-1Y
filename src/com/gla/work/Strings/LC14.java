abf342eecc356c97a81ec68ec58aa9911bf59e1d
imshiv1907-coder <imshiv1907@gmail.com>
2026-02-09 22:21:49 +0530

    Class work of 09/02/26

---
+++ src/com/gla/work/Strings/LC14.java
@@ -0,0 +1,26 @@
+package com.gla.work.Strings;
+
+public class LC14 {
+
+        public String longestCommonPrefix(String[] strs) {
+            Arrays.sort(strs);
+            int i=0;
+            String ans="";
+            while(i<strs[0].length()&&i<strs[strs.length-1].length()){
+                if(strs[0].charAt(i)!=strs[strs.length-1].charAt(i)){
+                    return ans;
+                }
+                else {
+                    char ch = strs[0].charAt(i);
+                    ans+=ch;
+                }
+                i++;
+            }
+            return ans;
+        }
+    public static void main(String[] args) {
+    }
+
+    {
+    }
+}
